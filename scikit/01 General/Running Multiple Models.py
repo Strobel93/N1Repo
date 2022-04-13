753e7e3c25a4599df4dff767341c5b53baacf65e
strobelt <strobel1993@web.de>
2022-04-13 06:57:42 +0200

    General Update

--- scikit/01 General/Running Multiple Models.py
+++ scikit/01 General/Running Multiple Models.py
@@ -9,22 +9,25 @@ from sklearn import model_selection
 #########################################################################################
 X_train = X_test = y_train = y_test = 0
 names = []
+predictions = []
 scoring = ['accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted', 'roc_auc']
 models = [
-          ('LogReg', LogisticRegression()),
-          ('RF', RandomForestClassifier()),
-          ('KNN', KNeighborsClassifier()),
-          ('SVM', SVC()),
-          ('GNB', GaussianNB()),
-        ]
+    ('LogReg', LogisticRegression()),
+    ('RF', RandomForestClassifier()),
+    ('KNN', KNeighborsClassifier()),
+    ('SVM', SVC()),
+    ('GNB', GaussianNB()),
+]
 
 for name, model in models:
-        kfold = model_selection.KFold(n_splits=5, shuffle=True, random_state=90210)
-        cv_results = model_selection.cross_validate(model, X_train, y_train, cv=kfold, scoring=scoring)
-        clf = model.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-        names.append([name, cv_results])
+    # Set up 5 different train/test splits of same data
+    k_fold = model_selection.KFold(n_splits=5, shuffle=True, random_state=90210)
+    # Cross Validate the current model using different/multiple scoring systems
+    cv_results = model_selection.cross_validate(model, X_train, y_train, cv=k_fold, scoring=scoring)
+    # Train Model and Predict values (optional?)
+    clf = model.fit(X_train, y_train)
+    y_predicts = clf.predict(X_test)
 
-
-
-
+    # Append Results: Scores and Predictions
+    predictions.append([name, y_predicts])
+    names.append([name, cv_results])
