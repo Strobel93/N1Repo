c2e9c42aa494e54693447f388edcbdea50597f0d
strobelt <strobel1993@web.de>
2022-04-19 11:04:56 +0200

    SGD

--- scikit/03 Classification/Smaller Datasets.py
+++ scikit/03 Classification/Smaller Datasets.py
@@ -1,12 +1,28 @@
+from sklearn.linear_model import SGDClassifier
+from sklearn.datasets import load_iris
 ######################################################################################
 # First and Second Prio
     # SGD Classifier
     # Kern Approximation
 ######################################################################################
 # SGD Classifier
+########################
+iris = load_iris()
+
+# Take first 2 features (relevant for distance calculation)
+x = iris.data[:, :2]
+y = iris.target
+
+sgdc = SGDClassifier(loss="hinge", alpha=0.01, max_iter=200)
+sgdc.fit(x,y)
+
+# print(sgdc.predict(x[-15:]))
+# print(y[-15:])
 
 
-
+########################
+# Kernel Approximation
+########################
 
 
 
