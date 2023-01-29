6861567aea3ce175c6b62dd58722d6b8d90456f4
Tobias Strobel <strobel1993@web.de>
2023-01-29 11:54:37 +0100

    more c# basics

--- C#/C# Basics.cs
+++ C#/C# Basics.cs
@@ -1,6 +1,4 @@
-﻿https://www.w3schools.com/cs/cs_method_parameters.php
-
-/*############################################################################
+﻿/*############################################################################
  Basics and Theory
 ############################################################################*/
 /* C#
@@ -13,7 +11,9 @@
         -- ; at the end of codelines
         -- Main Method is the entry point, aka first method to be called
         -- Operators: ==, !=, ||, &&, ! (not, aka is not reverse --> !(x=5))
-
+        -- "static" Keyword: 
+            --can be called without instanciation of the object
+            --faster than non static objects, due to internal pointer overhead
 */
 //############################################################################
 // Variables (int,long   float,double   char,string   bool)
@@ -96,6 +96,174 @@ namespace ActualProgram
         public static void CallOtherMethod()
         {
             NamespaceName.ExampleClass.ExampleMethod();
+        }
+    }
+}
+
+//############################################################################
+//Methods
+//############################################################################
+//simple param
+static void ParamMethod(string param = "Default", int param2)
+{
+    Console.WriteLine(param + param2);
+}
+
+ParamMethod("CallWithParameter",69);
+
+//named param, key: value (ignore order)
+static void NamedParamMethod(int p1, int p2, int p3)
+{
+    Console.WriteLine("The last param is" + p3); 
+}
+
+NamedParamMethod(p3: 69);
+
+//return 
+static void ReturnMethod(int ret)
+{
+    return ret + 69;
+}
+
+int mf = ReturnMethod(420);
+
+//Overloading: methods can have the same name if number and/or type of parameters are different
+static void Adding(int x, int y)
+{
+    return x + y;
+}
+
+static void Adding(double x, double y)
+{
+    return x + y;
+}
+
+Adding(69+420);
+Adding(1.1 + 2.2);
+
+
+//############################################################################
+//Classes
+//############################################################################
+//Simple class without constructor
+class Base
+{
+    public int x = 69;
+}
+Base obj = New Base();
+obj.x;
+
+//Simple class with constructor function to overwrite base constructor 
+//classname = constructor name
+class Base
+{
+    //Fieldlist
+    public int F1;
+    public int F2;
+
+    //Constructor
+    public Base(string P1, string P1)
+    {
+        this.F1 = P1;
+        this.F2 = P2;
+    }
+}
+
+Base obj1 = new Base(69, 420);
+
+//Getter and Setter
+class GetSetClass
+{
+    //get { return Year; }   // get method
+    //set { Year = value; }  // set method
+    public int PublicYearOfBirth { get; set; }
+    private int PrivateYearOfBirth { get; set; }
+    public int MixYearOfBirth { get; private set; }
+
+    public int DependentYearOfBirth
+    {
+        get { return PublicYearOfBirth; }
+        set
+        {
+            if (value > 1900 || value < 3000)
+            { PublicYearOfBirth = value; }
+        }
+    }
+
+
+    public GetSetClass(int p1, int p2, int p3, int p4)
+    {
+        this.PublicYearOfBirth = p1;
+        this.PrivateYearOfBirth = p2;
+        this.MixYearOfBirth = p3;
+        this.DependentYearOfBirth = p4;
+    }
+
+}
+
+class execution
+{
+    public static void Main()
+    {
+        GetSetClass newGSClass = new GetSetClass(1, 2, 3, 2000);
+
+        //Public
+        newGSClass.PublicYearOfBirth = 69;
+        Console.WriteLine(newGSClass.PublicYearOfBirth);
+        Console.WriteLine(newGSClass.MixYearOfBirth);
+
+        //Private: can only be accessed within class (in this case constructor?)
+
+    }
+
+}
+
+//Inheritance
+class Vehicle  // base class (parent) 
+{
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
+}
+
+class Car : Vehicle  // derived class (child)
+{
+    public string modelName = "Mustang";  // Car field
+}
+
+Car vroom = new Car();
+vroom.modelName;
+vroom.brand;
+vroom.honk();
+
+//############################################################################
+//Static
+//############################################################################
+//Accessing:
+//Static can be accesed without any class instanciation
+//Non Static can be accessed once the class has been instanciated
+namespace StaticNonStaticDemo
+{
+    class Example
+    {
+        int x; // Non-Static Variable
+        static int y = 200; //Static Variable
+        public Example(int x)
+        {
+            //Initializing Non-Static Variable
+            this.x = x;
+        }
+        static void Main(string[] args)
+        {
+            //Static
+            Console.WriteLine(Example.y);
+
+            //Non Static
+            //Creating Object1
+            Example obj = new Example(50);
+            Console.WriteLine(obj.x);
         }
     }
 }
