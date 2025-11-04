4e88fbba2e78a639cafa4f59b143aa35b72659cc
Strobel <strobel1993@web.de>
2025-11-03 16:30:55 -0800

    CodeForces 3D

--- TrainingCodeForces/CF_3/d.py
+++ TrainingCodeForces/CF_3/d.py
@@ -1,4 +1,19 @@
-# https://codeforces.com/problemset/problem/1/A
+# https://codeforces.com/problemset/problem/3/D
 # Inputs:
+bracket_list = ['(','?','?',')']
 
+def handle_bracket(bracket: list):
+    #it is cheaper to close instead of open, so try close first
+    open_count = 0
+    for counter, check in enumerate(bracket):
+        if check == '(':
+            open_count += 1
+        if check == ')':
+            open_count -= 1
+       
+        if check == '?':
+             bracket[counter] = ')' if open_count > 0 else '('
+             open_count = open_count-1 if open_count > 0 else open_count+1
+    print(bracket)    
 
+handle_bracket(bracket_list)    
